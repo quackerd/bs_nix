08508bc9405e63469092815fe856b0924c9a94e2
op52 <op52@hardfault.org>
2018-12-20 10:12:46 +0800

    ok

--- setupo_laptop_undervolt.sh
+++ setupo_laptop_undervolt.sh
@@ -8,7 +8,7 @@ sudo pip install undervolt
 
 echo "Applying undervolt"
 sudo cp _undervolt /etc/systemd/system/undervolt.service
-sudo cp _undervolt_timer /etc/systemd/system/undervolt-timer.service
+sudo cp _undervolt_timer /etc/systemd/system/undervolt.timer
 
-sudo systemctl enable undervolt-timer
-sudo systemctl start undervolt-timer
+sudo systemctl enable undervolt.timer
+sudo systemctl start undervolt.timer
