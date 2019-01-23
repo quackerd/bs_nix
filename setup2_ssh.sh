e43e7edf408dddf68fc32aa6b0d17cfce621b638
Op52 <op52@hardfault.org>
2019-01-22 23:46:18 -0500

    fix

--- setup2_ssh.sh
+++ setup2_ssh.sh
@@ -5,6 +5,9 @@ sudo apt install keychain
 
 echo "Copying private key..."
 cp id_op52 ~/.ssh/id_op52
+cp id_op52.pub ~/.ssh/id_op52.pub
+chmod 600 ~/.ssh/id_op52
+chmod 644 ~/.ssh/id_op52.pub
 
 echo "Configuring keychain..."
 echo "# Configured for keychain" >> ~/.bashrc
