0d89c1f770fc6a1a40a9ae69f4e5a0afada18724
Op52 <op52@hardfault.org>
2019-01-22 23:21:44 -0500

    ssh

--- setup2_gpg.sh
+++ setup2_gpg.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-KEY_GRIP=5351806D27F0E18166370620F4ED903E973C03C6
+echo "Installing keychain..."
+sudo apt install keychain
 
-echo "Importing keys"
-gpg --import gpgkey.pub
-gpg --import gpgkey.pri
+echo "Copying private key..."
+cp id_op52 ~/.ssh/id_op52
 
-echo "Adding ssh support"
-echo "enable-ssh-support" >> $HOME/.gnupg/gpg-agent.conf
-cat _gpg_bashrc >> $HOME/.bashrc
-echo $KEY_GRIP >> $HOME/.gnupg/sshcontrol
-
-echo "Restarting gpg-agent"
-gpgconf --kill gpg-agent
-gpgconf --launch gpg-agent
-
-echo "Please use \"gpg --editkeys key_id\" to trust your keys"
+echo "Configuring keychain..."
+echo "# Configured for keychain" >> ~/.bashrc
+echo "eval \`keychain --eval --agents ssh id_op52\`" >> ~/.bashrc
