aa807b928659611471011031d78ee3ec916501d8
QuackeR <qs@quacker.org>
2019-06-05 23:42:01 -0400

    zsh

--- setup1_zsh.sh
+++ setup1_zsh.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-echo "Installing zsh and fonts..."
-sudo dnf install zsh powerline-fonts
+echo "Installing zsh..."
+sudo dnf install -y zsh git
 
 echo "Installing omzsh..."
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-echo "Changing default shell..."
-chsh /bin/zsh
-
 echo "Configuring zsh..."
-cat ~/.zshrc | sed 's/SSH_KEY_PATH=.*/SSH_KEY_PATH=\"~\/.ssh\/id_ed25519\"/g' | sed 's/plugins=.*/plugins=\(git ssh-agent\)/g' | sed 's/ZSH_THEME=.*/ZSH_THEME=\"agnoster\"/g' > ~/.zshrc
+cat ~/.zshrc | sed 's/SSH_KEY_PATH=.*/SSH_KEY_PATH=\"~\/.ssh\/id_ed25519\"/g' | sed 's/plugins=.*/plugins=\(git ssh-agent zsh-autosuggestions zsh-syntax-highlighting\)/g' | sed 's/ZSH_THEME=.*/ZSH_THEME=\"gentoo\"/g' > ~/.zshrc
 
-echo "Adding custom commands..."
-echo "alias update-grub=\"sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg\"" >> ~/.zshrc
-echo "alias remove-kern=\"sudo dnf remove \$(dnf repoquery --installonly --latest-limit=-1 -q)\"" >> ~/.zshrc
-
-
+echo "Adding custom plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
