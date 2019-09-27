6bf6da88c97a3b9c63d1b1664b8499504efe20c6
quackerd <d@quacker.org>
2019-09-27 00:52:32 -0400

    ssh

--- 3_ssh.sh
+++ 3_ssh.sh
@@ -3,8 +3,7 @@
 echo "Configuring SSH..."
 
 mkdir -p ~/.ssh
-cp _ssh_config ~/.ssh/config 
-cp _ssh_pub ~/.ssh/authorized_keys
+cat ssh_pub >> ~/.ssh/authorized_keys
 chmod 700 ~/.ssh
 chmod 644 ~/.ssh/authorized_keys
 chmod 644 ~/.ssh/config
